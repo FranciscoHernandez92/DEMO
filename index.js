8788c324dfa100cff63e00dae95684017380a462
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-14 14:46:54 +0100

    Add index modified

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function multiplicate (num1, num2) {
 
        // Arrrow function 
 const mutliply = (x, z) => {
-  console.log(x * z);
+  return x * z;
 }
 mutliply(6,9)
 // 8. Crea una función que imprima por consola el resultado de elevar al cubo un número dado como parámetro.
@@ -46,7 +46,8 @@ third(3)
 
       //arrow function 
 const elevateCube = (m) => {
-    return m**3  //console.log(Math.pow(a, 3))
+  const result = m **3
+    return result  //console.log(Math.pow(a, 3))
 }
 elevateCube(5)
 // 9. Crea una función que saque por consola el área de un rectángulo de dimensiones dadas como parámetro.
@@ -85,30 +86,43 @@ if(randomNumber === userNumber){
 }
 app()
 // 12. Crea una función que reciba un número como parámetro e imprima por consola si el número dado es par o impar.
-const correctNumber = (p) => {
-  if (p % 2) {
+const isEvenOrOdd = (p) => {
+  if (p % 2 === 0) {
     console.log(`El numero ${p} es impar`)
-     return
-  `El numero ${p} es par`;
+     return `El numero ${p} es par`;
   }
 }
-correctNumber(115)
+isEvenOrOdd(8)
+               // Otra manera
+const checkEvenOrOdd = (p1) => {
+  return isEvenOrOdd(n) ? 'Even' : 'Odd'
+}
+console.log(checkEvenOrOdd(6))
 // 13. Crea una función que reciba un parámetro de tipo string e imprima por consola el string revertido. (ejemplo: 'casa' => 'asac).
 function reverse(word) {
-  const change = word.split('').reverse().join('')
+  const change = word.split('').reverse().join('')  //.split convierte la palabra en array. .reverse le da la vuelta. .join lo vuelve a convertir en string.
   return change
 }
 reverse('teide')
+              // Otra forma
+const reverse1 = (s) => {
+  let result = ''
+  for (let i = 0; i < s.length; i++) {
+    result = s[i] + result
+  }
+  return result
+}
+console.log(reverse('mi casa'));
 // 14. Crea una función que devuelva la tabla de multiplicar de un número introducido como parámetro.
 // LLama a la función desde console.log() para que el resultado se imprima por consola
-const num3 = (t) =>{
-let result=[];
-for(let i = 0; i < 11; i++){
-  result.push(t * i);
+const table = (t) =>{
+let result='';
+for( let i = 1; i <= 10; i++){
+  result += `${t} * ${i} = ${t * i}\t`
 }
 return result;
 };
-num3(9)
+console.log(table(4))
 // 15. Crea una función que reciba un número por parámetros y retorne si el número recibido es un número primo.
 // LLama a la función desde console.log() para que el resultado se imprima por consola
 const isPrime = (h) => {
@@ -123,4 +137,49 @@ const isPrime = (h) => {
     return true 
   }
 
-console.log(isPrime(3))
+console.log(isPrime(4))
+     //Otra Forma
+const isPrime1 = (h) => {
+  for (let j = 2; j < h; j++) {
+    if (h % j === 0){
+      return false
+    }
+  }
+  return h > 1
+}
+console.log(isPrime1(3))
+console.log(isPrime1(4));;
+// const checkIsPrimeOrNot = () => {
+//  return isPrime(h) ? `${n} es primo` : `${n} no es primo` 
+// }
+
+//16.Crea una funcion que reciba un numero como parametro y devuelva el factorial de ese numero.
+//Llama a la funcion desde console.log() para que el resultado se imprima por consola.
+const factorial = (f) =>{
+  let result = 1;   //Acumulador y se tiene que inicializar. En multiplicacion empieza por 1, en suma empieza por 0.
+  for (i = 1; i <= f; i++){
+    result = result * i;  // Otra forma: result * = i
+  }
+  return result
+}
+console.log(factorial(5));
+console.log(factorial(-3));
+
+
+         //Otra forma para que no aparezca negativos ni decimales.
+const factorial1 = (f) =>{
+  let result = 1;  
+  if(f < 0 ) {  //Si es menor que cero no entra en la funcion y devuelve directamente el mensaje antes de la funcion.
+    return 'El factorial solo se calcula en numeros enteros y positivos, idiota'
+  }
+  if (Math.trunc(f) !== f){ //Te quita los decimales
+    return 'El factorial solo se calcula en numeros enteros y positivos, idiota'
+  }
+    for (i = 1; i <= f; i++){
+      result = result * i;  
+    }
+    return result
+}
+console.log(factorial1(5));
+console.log(factorial1(-6));
+console.log(factorial(1.5));
